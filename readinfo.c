09a448e9c74cd47ec2c4c3e0fee64c6da46bfc9e
SaWKiri <swissa22@gmail.com>
2017-06-21 12:44:34 +0300

    files added to rep

---
+++ readinfo.c
@@ -0,0 +1,280 @@
+/*
+*
+* Helper program to read cpu info and memory for
+* sys_status_module.
+*
+* this app will read from /proc and calculate ram use in the system and cpu usage
+* this info will be send to sys_status_module.
+*
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/klog.h>
+
+
+
+#define READ_BUFFER 128
+#define NEW_LINE 10
+#define ASCII_OF_ZERO 48
+#define ASCII_OF_NINE 57
+#define ASCII_OF_SPACE 32
+#define ASCII_OF_NEW_LINE 10
+#define SUCCESS 0
+#define ERR_READ_FAIL -1
+#define ERR_WRITE_FAIL -2
+#define ERR_OPEN_FAIL -3
+
+double read_mem(void)
+{
+  int fd,i=0, j=0, _total = 0, _free = 0;
+  double memory_used_precentage = 0;
+  char buffer[READ_BUFFER];
+  char memory_data[READ_BUFFER], total_memory[READ_BUFFER]={0},mem_free[READ_BUFFER]={0};
+
+  fd = open("/proc/meminfo",O_RDONLY);
+
+  if(fd==0)
+  {
+    return ERR_OPEN_FAIL;
+  }
+
+  flock(fd,LOCK_EX);
+  if(read(fd,&buffer,READ_BUFFER) <= 0)
+  {
+    flock(fd,LOCK_UN);
+    return ERR_READ_FAIL;
+  }
+  flock(fd,LOCK_UN);
+
+  //read first line - total memory
+  while(buffer[++i] != ASCII_OF_NEW_LINE)
+  {
+    if(buffer[i] >= ASCII_OF_ZERO && buffer[i] <= ASCII_OF_NINE)
+    {
+      total_memory[j++]=buffer[i];
+    }
+  }
+  total_memory[j] = '\0';
+  j=0;
+
+  //skipping the 2nd row
+  i = i + 28;
+
+  //readind available memory
+  while(buffer[++i] != ASCII_OF_NEW_LINE)
+  {
+    if(buffer[i] >= ASCII_OF_ZERO && buffer[i] <= ASCII_OF_NINE)
+    {
+      mem_free[j++]=buffer[i];
+    }
+  }
+  mem_free[j] = '\0';
+
+  //convert to string to int
+  _total = atoi(total_memory);
+  _free = atoi(mem_free);
+
+
+  printf("\n\nmemory usage: %lf\n",100.0*((double)(_total - _free)/((double) _total)) );
+  memory_used_precentage = ((double)(_total - _free) / (double)_total) * 100.0;
+  printf("memory calculated: %lf\n",memory_used_precentage);
+
+return memory_used_precentage;
+}
+
+int write_memory(int memory)
+{
+  int fd;
+  char precentege_as_array[READ_BUFFER] = { 0 };
+
+  fd = open("/sys/sys_status_module/memory",O_WRONLY);
+
+  if(fd == 0)
+  {
+    printf("%s\n","failed open sysfs memory" );
+    return ERR_OPEN_FAIL;
+  }
+
+  //converting integer to string
+  sprintf(precentege_as_array,"%d",memory);
+
+  flock(fd,LOCK_EX);
+  if(write(fd,precentege_as_array,sizeof(char)*strlen(precentege_as_array)) < 0)
+  {
+    printf("write failed: failed to store memory");
+    flock(fd,LOCK_UN);
+    close(fd);
+    return ERR_WRITE_FAIL;
+  }
+  flock(fd,LOCK_UN);
+
+  //closing file
+  close(fd);
+
+ //returning number of byte write
+  return sizeof(char)*strlen(precentege_as_array);
+
+
+}
+
+//previos of cpu snapshot - to more accuratly calculate cpu usage
+static long int PrevIdle = 0 , PrevTotal =0 , PrevNonIdle = 0;
+
+int read_cpu(void)
+{
+  char buffer[READ_BUFFER],number[READ_BUFFER];
+  long int cpu_data[10] = {0};
+  int fd;//file descriptor for reading cpu stat from /proc/stat
+  int i=5,j=0,k=0;
+
+  long int  Idle , Total , NonIdle;
+  long int totald =0 , idled =0;
+
+  int cpu_usage_in_precentage;
+
+  if((fd = open("/proc/stat",O_RDONLY)) == 0)
+  {
+    close(fd);
+    return ERR_OPEN_FAIL;
+  }
+
+  flock(fd,LOCK_EX);
+  if(read(fd,&buffer,READ_BUFFER) < 0)
+  {
+    flock(fd,LOCK_UN);
+    close(fd);
+    return ERR_READ_FAIL;
+  }
+  flock(fd,LOCK_UN);
+
+  /*
+  * copy first line.
+  * skiping the 5 first character (i=4), it will be allways "cpu  "
+  * reading each number as a string and converting them to integer
+  * all number going into cpu[] array in the same read order
+  */
+  while(buffer[i] != NEW_LINE)
+  {
+
+    if((buffer[i] >= ASCII_OF_ZERO && buffer[i] <= ASCII_OF_NINE) || buffer[i] == ASCII_OF_SPACE)
+    {
+      number[j++] = buffer[i];
+    }
+
+    i++;
+    /*
+    * if space or new line has been read reading number has ended
+    * convering the string to number
+    * resetting number string to 0
+    */
+    if(buffer[i] == ASCII_OF_SPACE || buffer[i] == ASCII_OF_NEW_LINE)
+    {
+      number[j] = '\0';//putting end string in the end of the number for atoi()
+      cpu_data[k++] = atol(number);//convert string number to long int
+      j=0;
+      //0-ing "number" array for next string to read
+      memset(number, 0, READ_BUFFER);
+    }
+
+  }//end of while loop
+
+  Idle = cpu_data[3] + cpu_data[4];
+  NonIdle = cpu_data[0] + cpu_data[1] + cpu_data[2] + cpu_data[5] + cpu_data[6] + cpu_data[7];
+
+  Total = Idle + NonIdle;
+
+  totald = Total - PrevTotal;
+  idled = Idle - PrevIdle;
+  printf("debug: totald: %ld, idled: %ld\ntotald - idled = %ld\nprecent:%lf\n",totald,idled,totald - idled, 100.0*((double)(totald-idled))/((double)(totald)));
+  cpu_usage_in_precentage = 100.0*((double)(totald-idled))/((double)(totald));
+
+  //saving last snapshot
+  PrevIdle = Idle;
+  PrevTotal = Total;
+
+  printf("cpu precent as int:%d\n",cpu_usage_in_precentage);
+  return cpu_usage_in_precentage;
+}
+
+int write_cpu(int cpu)
+{
+  int fd,j=0;
+  char precentege_as_array[READ_BUFFER];
+
+  fd = open("/sys/sys_status_module/cpu",O_WRONLY);
+
+  if(fd == 0)
+  {
+    return ERR_OPEN_FAIL;
+  }
+
+  //converting integer to string
+  sprintf(precentege_as_array,"%d",cpu);
+  flock(fd,LOCK_EX);
+  if(write(fd,precentege_as_array,sizeof(char)*strlen(precentege_as_array)) < 0)
+  {
+
+    flock(fd,LOCK_UN);
+    close(fd);
+    return ERR_WRITE_FAIL;
+  }
+  flock(fd,LOCK_UN);
+  //closing file
+  close(fd);
+
+ //returning number of byte write
+  return sizeof(char)*strlen(precentege_as_array);
+
+
+}
+
+
+int main(int argc , char* argv[])
+{
+  /* variable to hold the system status read from /proc*/
+  double cpu_usage_in_precentage = 0;
+  double memory_usage_in_precentage = 0;
+  int to_run = 0;
+  int fd;
+  char buffer =  0 ;
+
+
+
+  do
+  {
+    fd=open("/sys/sys_status_module/to_run",O_RDWR);
+
+    flock(fd,LOCK_EX);
+    read(fd,&buffer,1);
+    flock(fd,LOCK_UN);
+	  close(fd);
+    to_run = atoi(&buffer);
+
+    memset(&buffer,0,1);
+
+    klogctl(10,"test",4);
+    cpu_usage_in_precentage = read_cpu();
+
+
+   write_cpu(cpu_usage_in_precentage);
+
+
+    memory_usage_in_precentage = read_mem();
+
+
+    write_memory(memory_usage_in_precentage);
+
+    sleep(1);
+
+
+  }while(to_run == 0);
+
+return SUCCESS;
+}
---
+++ sys_status_module.c
@@ -0,0 +1,498 @@
+/*
+*
+* Advance OS 2 project - oron swissa nerya yona
+*
+*/
+
+/*
+TODO
+  add led and button
+  see if all work together ???
+
+*/
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/kernel.h>
+#include <linux/jiffies.h>
+#include <linux/fs.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/kobject.h>
+#include <linux/init.h>
+#include <linux/keyboard.h>
+#include <linux/semaphore.h>
+#include <linux/types.h>
+#include <linux/timer.h>       /* Needed for timer */
+#include <linux/gpio.h>       // Required for the GPIO functions
+
+
+
+
+#define DRIVER_AUTHOR "Oron Swissa and nerya yona"
+#define DRIVER_DESC "Course Project - System status Led"
+
+#define CPU_VALUE_ATTR_NAME "cpu"
+#define MEMORY_VALUE_ATT_NAME "memory"
+#define TO_RUN_VALUE_ATT_NAME "to_run"
+#define STATUS_OBJECT_NAME "sys_status_module"
+
+#define SECONDS_COUNT 5
+
+/*
+* leds and button config
+*/
+static unsigned int gpioLED27 = 27;           ///< Default GPIO for the LED is 27
+static bool ledOn27 = 0;
+
+static unsigned int gpioLED5 = 5;
+static bool ledOn5 = 0;
+
+static unsigned int gpioLED6 = 6;
+static bool ledOn6 = 0;
+
+static unsigned int gpioLED13 = 13;
+static bool ledOn13 = 0;
+
+/*
+* timer setup
+*/
+/* setup timer */
+struct timer_list myTimer;
+int i;
+
+
+//sysfs entry declartion
+static struct kobject *mykobj;
+/*
+* the attribute that will be shown in the sysfs
+*/
+static int memory =  0;
+static int cpu = 0;
+static int to_run = 0;
+static bool to_show_cpu = 0;
+static int precent = 0;
+
+/*
+* running user application from kernel space
+* this should be calld from button press for now on init
+* it will be called
+*/
+static int run_user_app(void)
+{
+  struct subprocess_info *sub_info;
+	///home/saw/Desktop/os2/project
+
+  //  /home/saw/Desktop/AdvanceOS/lkp/project/a.out
+	// /home/pi/Desktop/project/a.out
+	char *argv[] = {"/home/saw/Desktop/AdvanceOS/lkp/project/a.out", NULL};
+  static char *envp[] = {"HOME=/","TERM=linux","PATH=/sbin:/bin:/usr/sbin:/usr/bin",NULL};
+
+  sub_info = call_usermodehelper_setup(argv[0],argv,envp,GFP_ATOMIC,NULL,NULL,NULL);
+  if(sub_info == NULL) return -1;
+
+  return call_usermodehelper_exec(sub_info,UMH_WAIT_PROC);
+
+}
+
+//starts the timer
+static void my_set_timer(struct timer_list * mtimer)
+{
+	 mtimer->expires = jiffies + (HZ*SECONDS_COUNT);
+    add_timer (mtimer); /* setup the timer again */
+}
+
+// timer interrupt sevice routine - executes timer action
+void timerFun (unsigned long arg) {
+
+    int tmp,run =42;
+    i++;
+    tmp = i;
+    printk (KERN_INFO "Called timer %d times\r\n", tmp);
+
+		printk(KERN_INFO "Running run_user_app!!");
+		run =  run_user_app();
+		printk(KERN_INFO "run user app return: %d",run);
+
+    if(to_show_cpu == 0)
+    {
+      precent = cpu;
+	    printk(KERN_INFO "sys status module: show cpu");
+    }
+    else
+    {
+      precent = memory;
+	  printk(KERN_INFO "sys status module: show memory");
+    }
+
+    //calculating how much led to turn on base on precentage
+      if(precent <= 25)
+      {
+		printk(KERN_INFO "sys status module: gpio27 on");
+        ledOn27 = false;
+        //led on
+        gpio_set_value(gpioLED27, ledOn27);
+        //led off
+        gpio_set_value(gpioLED5, true);gpio_set_value(gpioLED6, true);gpio_set_value(gpioLED13, true);
+      }
+      else if( precent > 25 && precent <= 50)
+      {
+		printk(KERN_INFO "sys status module: gpio27 and gpio5 on");
+        ledOn27 = false;
+        ledOn5 = false;
+        //led on
+        gpio_set_value(gpioLED27, ledOn27);gpio_set_value(gpioLED5, ledOn5);
+        //led off
+        gpio_set_value(gpioLED6, true);gpio_set_value(gpioLED13, true);
+      }
+      else if( precent > 50 && precent <= 75)
+      {
+		printk(KERN_INFO "sys status module: gpio27 & gpio5 & gpio6 on");
+        ledOn27 = false;
+        ledOn5 = false;
+        ledOn6 = false;
+        //led on
+        gpio_set_value(gpioLED27, ledOn27);gpio_set_value(gpioLED5, ledOn5);gpio_set_value(gpioLED6, ledOn6);
+        //led off
+        gpio_set_value(gpioLED13, true);
+      }
+      else if( precent > 75 && precent <= 100)
+      {
+		   printk(KERN_INFO "sys status module: all gpio led on!");
+        ledOn27 = false;
+        ledOn5 = false;
+        ledOn6 = false;
+        ledOn13 = false;
+        //led on
+        gpio_set_value(gpioLED27, ledOn27);
+        gpio_set_value(gpioLED5, ledOn5);
+        gpio_set_value(gpioLED6, ledOn6);
+        gpio_set_value(gpioLED13, ledOn13);
+      }
+
+
+
+    my_set_timer(&myTimer);
+
+}
+
+
+
+static struct attribute cpu_value = {
+  .name = CPU_VALUE_ATTR_NAME,
+  .mode = 0666,
+
+};
+
+static struct attribute memory_value = {
+  .name = MEMORY_VALUE_ATT_NAME,
+  .mode = 0666,
+};
+
+static struct attribute to_run_value = {
+  .name = TO_RUN_VALUE_ATT_NAME,
+  .mode = 0666,
+
+};
+
+static struct attribute *my_attrs[] = {
+  &cpu_value,
+  &memory_value,
+  &to_run_value,
+  NULL
+};
+
+/*
+* Function show will be called when u READ from sysfs attribute
+* 1 function for both attributes - print the value of the attribute
+* by there name.
+*/
+
+static ssize_t show(struct kobject *kobj, struct attribute *attr, char *buff)
+{
+  printk(KERN_INFO "System status Module : show called by %s attribute\n", attr->name);
+
+  if(strcmp(attr->name,CPU_VALUE_ATTR_NAME)==0)
+  {
+    sprintf(buff,"%d \n",cpu);
+    //by convention return number of byte printed
+    return sizeof(int);
+  }
+  if(strcmp(attr->name,MEMORY_VALUE_ATT_NAME)==0)
+  {
+    sprintf(buff,"%d \n",memory);
+    //by convention return number of byte printed
+    return sizeof(int);
+  }
+  if(strcmp(attr->name,TO_RUN_VALUE_ATT_NAME)==0)
+  {
+    sprintf(buff,"%d \n",to_run);
+    //by convention return number of byte printed
+    return sizeof(int);
+  }
+
+ return 0;
+
+
+}
+
+/*
+* Function store will be called when u WRITE to sysfs
+* 1 function for all attributes - store the value of the attribute
+* by there name.
+*/
+static ssize_t store(struct kobject *kobj,struct attribute *attr,const char *buff,size_t len)
+{
+  printk(KERN_INFO "System status Module : store called by %s attribute \n" ,attr->name);
+
+  if(strcmp(attr->name,CPU_VALUE_ATTR_NAME)==0)
+  {
+    int i=0;
+    i=sscanf(buff,"%d \n",&cpu);
+    //by convention return number of byte store
+    printk(KERN_INFO "Sys status module: store to cpu %d vars\n",i);
+    return sizeof(int);
+  }
+
+  if(strcmp(attr->name,MEMORY_VALUE_ATT_NAME)==0)
+  {
+    sscanf(buff,"%d \n",&memory);
+    //by convention return number of byte store
+    return sizeof(int);
+  }
+  if(strcmp(attr->name,TO_RUN_VALUE_ATT_NAME)==0)
+  {
+    sscanf(buff,"%d\n",&to_run);
+    //by convention return number of byte store
+    return sizeof(int);
+  }
+
+  return 0;
+}
+
+
+/*
+* connecting the Function show and store to the sysfs entry
+*/
+static struct sysfs_ops my_sysfs_ops = {
+  .show = show,
+  .store = store,
+};
+
+static struct kobj_type my_kobj_type = {
+  .sysfs_ops = &my_sysfs_ops,
+  .default_attrs = my_attrs,
+};
+
+/*
+* keyboard notifier
+*/
+
+struct semaphore sem;
+
+static const char* keymap[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
+                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
+                        "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
+                        "/", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
+                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
+                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
+                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+
+static const char* keymapShiftActivated[] =
+                        { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
+                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
+                        "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
+                        "?", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
+                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
+                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
+                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+
+static int shiftKeyDepressed = 0;
+
+//notifier function - callback
+int keylogger_notify(struct notifier_block *nblock, unsigned long code, void *_param)
+{
+    struct keyboard_notifier_param *param = _param;
+    if (code == KBD_KEYCODE)
+    {
+        if( param->value==42 || param->value==54 )
+        {
+            //acquire lock to modify the global variable shiftKeyDepressed
+            down(&sem);
+            if(param->down)
+                shiftKeyDepressed = 1;
+            else
+                shiftKeyDepressed = 0;
+            up(&sem);
+            return NOTIFY_OK;
+        }
+
+        if(param->down)
+        {
+            //acquire lock to read the global variable shiftKeyDepressed
+            down(&sem);
+            //if "p" or "P" presses switching to memory/cpu state
+            if(shiftKeyDepressed == 0)
+            {
+              if(keymap[param->value] == keymap[25])
+              {
+                to_show_cpu = !to_show_cpu;
+                printk(KERN_INFO "P has been pressed");
+              }
+                //printk(KERN_INFO "%s \n", keymap[param->value]);
+            }
+            else
+            {
+              if(keymapShiftActivated[param->value] == keymapShiftActivated[25])
+              {
+                to_show_cpu = !to_show_cpu;
+                printk(KERN_INFO "P has been pressed");
+              }
+                //printk(KERN_INFO "%s \n", keymapShiftActivated[param->value]);
+            }
+            up(&sem);
+        }
+    }
+
+    return NOTIFY_OK;
+}
+
+static struct notifier_block keylogger_nb =
+{
+    .notifier_call = keylogger_notify
+};
+
+
+
+/*
+* init
+*/
+static int __init init_sys_status(void)
+{
+  int err = -1;
+
+  //keybot notifier init
+  register_keyboard_notifier(&keylogger_nb);
+  printk(KERN_INFO "sys status module: Registering the keylogger module with the keyboard notifier list\n");
+  sema_init(&sem, 1);
+
+
+
+  //led and button init
+
+  gpio_request(gpioLED27, "green");          // gpioLED27 is 27 gpio by default, request it
+  gpio_direction_output(gpioLED27, 0);   // Set the gpio to be in output mode and turn on
+
+  gpio_request(gpioLED5,"yellow");
+  gpio_direction_output(gpioLED5, 0);
+
+  gpio_request(gpioLED6,"blue");
+  gpio_direction_output(gpioLED6, 0);
+
+  gpio_request(gpioLED13,"red");
+  gpio_direction_output(gpioLED13, 0);
+	printk(KERN_INFO "sys status module: gpio led have been added.");
+
+  mykobj = kzalloc(sizeof(*mykobj),GFP_KERNEL);
+  if(mykobj)
+  {
+    //getting error from gcc try on device or repalce to init and add
+    //kobj_init(mykobj,&my_kobj_type);
+    kobject_init(mykobj,&my_kobj_type);
+    if(kobject_add(mykobj,NULL,"%s",STATUS_OBJECT_NAME))
+    {
+      err = -1;
+      printk(KERN_INFO "sys status module: kobject_add() faild\n");
+      kobject_put(mykobj);
+      mykobj = NULL;
+    }
+
+    err = 0;
+
+
+  }
+
+  //timer init
+  init_timer (&myTimer);
+  myTimer.function = timerFun;
+  myTimer.data = 0;
+
+  my_set_timer(&myTimer);
+  printk (KERN_INFO "sys status module:timer added. \n");
+
+  printk(KERN_INFO "sys_status_module: init finished");
+  printk(KERN_INFO "sys_status_module: loaded\n");
+  return 0;
+
+}
+
+/*
+*
+*/
+static void __exit cleanup_sys_status(void)
+{
+  printk(KERN_INFO "sys_status_module: exit started");
+  unregister_keyboard_notifier(&keylogger_nb);
+  printk(KERN_INFO "Unregistered the keylogger module \n");
+
+  if(mykobj)
+  {
+    kobject_put(mykobj);
+    kfree(mykobj);
+  }
+  //free gpio
+  gpio_set_value(gpioLED27,0);
+  gpio_set_value(gpioLED5,0);
+  gpio_set_value(gpioLED6,0);
+  gpio_set_value(gpioLED13,0);
+
+  gpio_free(gpioLED27);
+  gpio_free(gpioLED5);
+  gpio_free(gpioLED6);
+  gpio_free(gpioLED13);
+	printk(KERN_INFO "sys status module: removed leds");
+
+  //free timer
+  if(!del_timer(&myTimer))
+  {
+	  printk(KERN_INFO "Couldn't remove timer");
+  }
+  else
+  {
+	  printk(KERN_INFO "Timer removed");
+  }
+  printk(KERN_INFO "sys_status_module unloaded and finished\n");
+
+
+
+}
+
+module_init(init_sys_status);
+module_exit(cleanup_sys_status);
+
+
+/*
+* module license
+*/
+MODULE_LICENSE("GPL");
+
+/*
+*   authors and description
+*/
+MODULE_AUTHOR(DRIVER_AUTHOR); // who wrote this module
+MODULE_DESCRIPTION(DRIVER_DESC);//what does this module do
+
+/*
+*This module uses /dev/testdevice. The MODULE_SUPPORTED_DEVICE macro might
+*be used in the futur to help automate configuration of modules, but is
+*currently unsued other than for documentation
+*/
+
+MODULE_SUPPORTED_DEVICE("Rasspebry pi 3");
